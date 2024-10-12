7705003ea182431a68010bd1c6c972eedaf2760b
Mario Edgar Márquez Vara <marioedgarmarquezvara@gmail.com>
2024-10-12 01:30:09 -0600

    Add the Composite Primary Key

---
+++ src/main/java/com/ghostappi/backend/model/CategoryCouponPK.java
@@ -0,0 +1,33 @@
+package com.ghostappi.backend.model;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class CategoryCouponPK implements Serializable {
+    private Coupon idCoupon;
+    //private Category idCategory
+    private Integer idCategory;
+    // @Override
+   /* public boolean equals(Object o) {
+        if (this == o) 
+        return true;
+        if (!(o instanceof CategoryCouponPK categoryCouponPK))
+            return false;
+        return idCategory.getId() == categoryCouponPK.idCategory.getId() && Objects.equals(idCoupon, categoryCouponPK.idCoupon);
+    }*/ 
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idCoupon, idCategory);
+    }
+
+    /*public Category getIdCategory() {
+        return idCategory;
+    }*/
+
+    public Coupon getIdCoupon() {
+        return idCoupon;
+    }   
+}
+
