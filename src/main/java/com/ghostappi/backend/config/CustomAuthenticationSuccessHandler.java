689b8c6eb2f035aa6cd5a91f10cd882a694327bf
Felix <cowboy.spaceboy123@gmail.com>
2024-11-27 21:56:58 -0600

    Authentication jwt plus

---
+++ src/main/java/com/ghostappi/backend/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,18 @@
+package com.ghostappi.backend.config;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(jakarta.servlet.http.HttpServletRequest request,
+            jakarta.servlet.http.HttpServletResponse response, Authentication authentication)
+            throws IOException, jakarta.servlet.ServletException {
+        response.sendRedirect("/doc/swagger-ui/index.html#");
+    }
+}
--- src/main/java/com/ghostappi/backend/config/SecurityConfig.java
+++ src/main/java/com/ghostappi/backend/config/SecurityConfig.java
@@ -10,23 +10,45 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
+
+@OpenAPIDefinition(info = @Info(title = "Ghost Supplements API", version = "v1"), security = @SecurityRequirement(name = "bearerAuth"))
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Autowired
     private AuthenticationProvider authenticationProvider;
+
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+
+    @SuppressWarnings("removal")
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/swagger-ui/**","/v3/api-docs/**").permitAll()
+                        .requestMatchers("/auth/**", "/login/**", "/swagger-ui/**", "/v3/api-docs/**",
+                                "/doc/swagger-ui/index.html#")
+                        .permitAll()
+                        .requestMatchers("/products/**",
+                                "/wallets/**")
+                        .hasRole("Cliente")
                         .anyRequest().authenticated())
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin()
+                .successHandler(customAuthenticationSuccessHandler);
 
         return http.build();
     }
